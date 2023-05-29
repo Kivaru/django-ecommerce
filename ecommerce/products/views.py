346e5408098dde4a9e0b85c87652ca4c47d48329
Godfrey Kivaru <kivarugodfrey@gmail.com>
2023-05-29 14:43:15 +0300

    first commit

---
+++ ecommerce/products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from products.models import Product
+from django.views.generic import ListView
+
+# Create your views here.
+
+class Home(ListView):
+    model = Product
+    template_name = 'products/home.html'
+    
